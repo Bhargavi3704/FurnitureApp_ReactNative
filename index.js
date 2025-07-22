c7393cad869a51935df576bbca1140c12314b988
Bhargavi Gutti <132123427+Bhargavi3704@users.noreply.github.com>
2025-07-22 12:26:22 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+99% of storage used … If you run out of space, you can't save to Drive or use Gmail. Get 30 GB of storage for ₹59.00 ₹0 for 1 month.
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
